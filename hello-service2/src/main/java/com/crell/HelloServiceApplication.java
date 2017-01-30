11cba5fac8ef6d39de9c612a6fd9c5d4b4fcb05f
陈奇 <chenqi@promote.cache-dns.local>
2017-01-30 18:52:28 +0800

    add

---
+++ hello-service2/src/main/java/com/crell/HelloServiceApplication.java
@@ -0,0 +1,15 @@
+package com.crell;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HelloServiceApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(HelloServiceApplication.class).web(true).run(args);
+	}
+
+}
